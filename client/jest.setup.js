a8f00b61c55b7bbccb922f6c2d9d558f1e9b347b
minhquanBr2 <minhquanhole03@gmail.com>
2024-05-29 14:44:29 +0700

    remove tests

--- client/jest.setup.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import 'vite/jest';
-
-global.mockAxios = new MockAdapter(axios);
-
-afterEach(() => {
-  global.mockAxios.reset();
-});
